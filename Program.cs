c82621a6bcc40c17e04b88a66da3f4b34be03b87
mn-6 <michal.nocun@studenci.collegiumwitelona.pl>
2026-04-24 21:38:35 +0200

    -

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class GeneratorLehmera
+{
+    static void Main()
+    {
+        //parametry
+        const long m = 2147483647; //modulo: 2^31 - 1 - liczba pierwsza Mersenne'a
+        const long a = 16807;      //mnoznik: MINSTD- Minimum Standard, generator Parka-Millera
+        
+        //pobranie danych
+        Console.Write("Ile liczb pseudolosowych wygenerowac? ");
+        if (!int.TryParse(Console.ReadLine(), out int count) || count <= 0)
+        {
+            Console.WriteLine("Prosze podac poprawna liczbe dodatnia.");
+            return;
+        }
+
+        //ustawiamy ziarno
+
+        //ta opcja jest zeby kazde uruchomienie bylo unikalne, bierze ilosc tickow systemu
+        //long x = DateTime.Now.Ticks % m;
+        //if (x == 0) x = 1;
+
+        //powtarzalna wersja
+        long x = 3;
+
+        Console.WriteLine($"\nGenerowanie {count} liczb (m={m}, a={a}, ziarno={x}):\n");
+
+        //generowanie liczb
+        for (int i = 0; i < count; i++)
+        {
+            //wzor X1= (a* X0) mod m
+            x = (a * x) % m;
+
+            //wynik
+            Console.WriteLine($"{i + 1}: {x}");
+        }
+    }
+}
